9eb8aefcf6d48b05b063fb9590e56c24271e892a
Aurélien Briot <briotaurelien88@gmail.com>
2024-10-14 09:50:22 +0200

    update classes

--- app/src/main/java/org/example/Center.java
+++ app/src/main/java/org/example/Center.java
@@ -2,13 +2,32 @@ package org.example;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="center")
 public class Center {
+    @Id
     private Integer id;
     private String adresse;
     private String ville;
 
+    @OneToMany
     private List<User> medecins;
+
+    @OneToMany(mappedBy = "timeslot")
     private List<Timeslot> creneaux;
+
+    @OneToOne
+    @JoinColumn(name = "id_administrateur",
+    foreignKey = @ForeignKey(name="administrateur_fk"),
+    nullable =  false)
     private User adminstrateur;
     
 }
--- app/src/main/java/org/example/Timeslot.java
+++ app/src/main/java/org/example/Timeslot.java
@@ -2,10 +2,21 @@ package org.example;
 
 import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "timeslot")
 public class Timeslot {
+    @Id
     private Integer id;
 
+    @ManyToOne
     private Center centre;
+
+    @ManyToOne
     private User patient;
 
     private Date date;
