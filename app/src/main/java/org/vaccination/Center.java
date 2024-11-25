a61263f4100c0576b608b1f19ca3f92dc8404b63
Aurélien Briot <briotaurelien88@gmail.com>
2024-11-25 10:29:36 +0100

    add attribute 'nom'

--- app/src/main/java/org/vaccination/Center.java
+++ app/src/main/java/org/vaccination/Center.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Table;
 public class Center {
     @Id
     private Integer id;
+    private String nom;
     private String adresse;
     private String ville;
 
@@ -31,13 +32,22 @@ public class Center {
 
     }
 
-    public Center(Integer id, String adresse, String ville, List<User> medecins, List<Timeslot> creneaux, User admin) {
+    public Center(Integer id, String nom, String adresse, String ville, List<User> medecins, List<Timeslot> creneaux, User admin) {
         this.id = id;
+        this.nom = nom;
         this.adresse = adresse;
         this.ville = ville;
         this.medecins = medecins;
         this.creneaux = creneaux;
         this.adminstrateur = admin;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return this.nom;
     }
 
     public void setAdresse(String adresse) {
