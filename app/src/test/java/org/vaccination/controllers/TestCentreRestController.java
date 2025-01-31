c1e1a12bb30ca1aaf49159b97da3790417af1f0e
Aurélien Briot <briotaurelien88@gmail.com>
2025-01-31 18:15:33 +0100

    Ajout de tests

--- app/src/test/java/org/vaccination/controllers/TestCentreRestController.java
+++ app/src/test/java/org/vaccination/controllers/TestCentreRestController.java
@@ -1,9 +1,14 @@
 package org.vaccination.controllers;
 
+import static org.mockito.ArgumentMatchers.eq;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,6 +31,9 @@ public class TestCentreRestController {
 
     @MockitoBean
     private CentreService centreService;
+
+    @Captor
+    private ArgumentCaptor<Centre> centreCaptor;
 
     @Test
     void itShouldGetCentre() throws Exception {
@@ -67,4 +75,51 @@ public class TestCentreRestController {
         .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
         .andExpect(MockMvcResultMatchers.jsonPath("$[1].ville").value("Metz"));
     }
+
+    @Test
+    void itShouldAddACentre() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/super-admin/centre").content("""
+                {
+                   "ville": "Vandoeuvre",
+                   "adresse": "2 rue Jean Lamour",
+                   "nom": "Centre de Vandoeuvre" 
+                }
+                """).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+
+        Mockito.verify(centreService).create(centreCaptor.capture());
+        Assertions.assertThat(centreCaptor.getValue().getVille()).isEqualTo("Vandoeuvre");
+        Assertions.assertThat(centreCaptor.getValue().getAdresse()).isEqualTo("2 rue Jean Lamour");
+        Assertions.assertThat(centreCaptor.getValue().getNom()).isEqualTo("Centre de Vandoeuvre");
+    }
+
+    @Test
+    void itShouldUpdateACentre() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/super-admin/centre/1").content("""
+                {   
+                    "id": 1,
+                    "ville": "Vandoeuvre"
+                }
+                """).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+
+        Mockito.verify(centreService).update(eq(1), centreCaptor.capture());
+        Assertions.assertThat(centreCaptor.getValue().getVille()).isEqualTo("Vandoeuvre");
+        Assertions.assertThat(centreCaptor.getValue().getId()).isEqualTo(1);
+    }
+
+    @Test
+    void itShouldDeleteACentre() throws Exception {
+        Centre centreMock = new Centre();
+        centreMock.setId(1);
+        centreMock.setVille("Nancy");
+    
+        Mockito.when(centreService.findOneById(1)).thenReturn(centreMock);
+        
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/super-admin/centre/1").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+
+        Mockito.verify(centreService).delete(eq(1));
+    }
+
+    
 }
